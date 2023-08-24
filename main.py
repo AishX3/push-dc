51eb115e7319709f51a673126948104bbc7e76eb
Aish <97154771+AishX3@users.noreply.github.com>
2023-08-24 19:19:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,30 @@
-import requests
+import discord
+from discord.ext import commands
 import random
-import time
-import os
-from colorama import Fore
 
-print("   ____          ____       _                    ")
-print("  | __ )  __ _  |  _ \ __ _| |_ ___ _ __   __ _  ")
-print("  |  _ \ / _' | | |_) / _' | __/ _ \ '_ \ / _' | ")
-print("  | |_) | (_| | |  __/ (_| | ||  __/ | | | (_| | ")
-print("  |____/ \__, | |_|   \__,_|\__\___|_| |_|\__, | ")
-print("         |___/                            |___/  \n")
-print("=================================================")
-author = "Bg.Pateng"
-print("Author: " + author)
-script = "Push Rank Discord"
-print("Script: " + script)
-telegram = "@bangpateng_group"
-print("Telegram: " + telegram)
-youtube = "Bang Pateng"
-print("Youtube: " + youtube)
-print("===========================================")
-print('PERINGATAN : TIDAK UNTUK DI PERJUAL-BELIKAN')
-print("===========================================\n")
+intents = discord.Intents.default()
+intents.presences = True
+intents.members = True
 
-time.sleep(1)
-
-channel_id = input("Masukkan ID channel: ")
-waktu2 = int(input("Set Waktu Kirim Pesan: "))  # Menyimpan waktu antara pengiriman pesan
-
-time.sleep(1)
-print("3")
-time.sleep(1)
-print("2")
-time.sleep(1)
-print("1")
-time.sleep(1)
-
-os.system('cls' if os.name == 'nt' else 'clear')
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 with open("pesan.txt", "r") as f:
-    words = f.readlines()
+    responses = f.readlines()
 
 with open("token.txt", "r") as f:
-    authorization = f.readline().strip()
+    bot_token = f.readline().strip()
 
-while True:
-    channel_id = channel_id.strip()
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
 
-    payload = {
-        'content': random.choice(words).strip()
-    }
+@bot.event
+async def on_message(message):
+    # Cek apakah pesan bukan dari bot sendiri dan dalam saluran yang diinginkan
+    if message.author != bot.user and message.channel.id == YOUR_CHANNEL_ID:
+        response = random.choice(responses).strip()  # Memilih pesan secara acak
+        await message.channel.send(response)
 
-    headers = {
-        'Authorization': authorization
-    }
+    await bot.process_commands(message)  # Penting untuk memproses perintah bot
 
-    r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", data=payload, headers=headers)
-    print(Fore.WHITE + "Sent message: ")
-    print(Fore.YELLOW + payload['content'])
-
-    time.sleep(waktu2)  # Menunggu waktu antara pengiriman pesan
+bot.run(bot_token)
