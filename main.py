c1170ee47b2dacbca0bc500a31946d6a6fdf03ff
Aish <97154771+AishX3@users.noreply.github.com>
2023-08-25 21:45:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import random
-import sys  # Import modul sys untuk mengakses argumen dari command line
 
 intents = discord.Intents.default()
 intents.presences = True
@@ -9,11 +8,12 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-with open("pesan.txt", "r") as f:
-    responses = f.readlines()
-
-with open("token.txt", "r") as f:
-    bot_token = f.readline().strip()
+responses = [
+    "Pesan balasan otomatis 1",
+    "Pesan balasan otomatis 2",
+    "Pesan balasan otomatis 3",
+    "Pesan balasan otomatis 4"
+]
 
 @bot.event
 async def on_ready():
@@ -21,18 +21,10 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    # Cek apakah pesan bukan dari bot sendiri dan dalam saluran yang diinginkan
-    if message.author != bot.user and message.channel.id == YOUR_CHANNEL_ID:
-        response = random.choice(responses).strip()  # Memilih pesan secara acak
+    if message.author != bot.user and message.channel.id == 976132165450473512:
+        response = random.choice(responses)
         await message.channel.send(response)
 
-    await bot.process_commands(message)  # Penting untuk memproses perintah bot
+    await bot.process_commands(message)
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python main.py <channel_id>")
-        sys.exit(1)
-
-    YOUR_CHANNEL_ID = int(sys.argv[1])  # Mengambil channel ID dari argumen command line
-
-    bot.run(bot_token)
+bot.run('OTMzNTg4MTg2OTc2ODkwOTIy.GhWKgt.5ZI6VIyFLSded0vTw6wArkbDV_kQUv1VhHT_qo')
